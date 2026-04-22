bac3e76452e17a6a7d1bd96363db307cee1f3a4b
safwanul sikder <186922998+Saf2717@users.noreply.github.com>
2026-04-22 22:13:39 +0100

    station closure bug fix

--- frontend/tfl-what-if-simulation/app/components/map/MapCanvas.jsx
+++ frontend/tfl-what-if-simulation/app/components/map/MapCanvas.jsx
@@ -375,7 +375,6 @@ export function MapCanvas() {
                 });
 
                 if (!cancelled) {
-                    closed.add("940GZZLUHAW"); // Harrow & Wealdstone, as you mentioned
                     setLiveClosedStations(closed);
                 }
             } catch (err) {
